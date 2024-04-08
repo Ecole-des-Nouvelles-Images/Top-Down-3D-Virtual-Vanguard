5143cb98577fdddddc03182a3af44e8070a9c816
charlieBoyer <charlieboyer@outlook.fr>
2024-04-08 18:05:21 +0200

    Finish first player movements
    + Install ProBuilder package
    * Update materials
    - Remove lighting and skybox for now
    * Nearly finish InputAction asset and start good PlayerController (now use CharacterController to move)
    * Player can enter/exit modules pretty nicely for now

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,105 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [Header("References")]
+        public Transform Convoy;
+        public PlayerInput Input;
+        public CharacterController Controller;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [Header("Settings")]
+        public float Speed = 1f;
+
+        [Header("Internal")]
+        public LayerMask ModuleLayer;
+        public float ModuleDetectionRadius;
+
+        private InputActionMap _convoy;
+        private InputActionMap _laser;
+        private InputActionMap _shield;
+        private InputActionMap _generator;
+        private InputActionMap _drones;
+
+        private Vector3 Position {
+            get => transform.position;
+            set => transform.position = value;
+        } 
+        private Vector3 _direction;
+
+        #region Debug
+
+        private Vector2 _value;
         
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(Position, ModuleDetectionRadius);
+            
+            Vector3 labelPosition = transform.position + Vector3.up * 1.5f;
+            Handles.Label(labelPosition, $"({_value.x:F3}, {_value.y:F3})");
+        }
+
+        #endregion
+
+        private void Awake()
+        {
+        }
+
+        private void Update()
+        {
+            Controller.Move(_direction);
+        }
+
+
+        #region Actions
+
+        public void OnMove(InputValue input)
+        {
+            Vector2 value = input.Get<Vector2>();
+            _value = value;
+            _direction = Convoy.right * (Mathf.Abs(value.x) >= Math.Abs(value.y) ? value.x : value.y) * Speed * Time.deltaTime;
+        }
+        
+        public void OnEnterModule()
+        {
+            Collider[] colliders = Physics.OverlapSphere(Position, ModuleDetectionRadius, ModuleLayer);
+
+            Collider closestModule = null;
+            float closestDistance = float.MaxValue;
+
+            foreach (Collider contact in colliders)
+            {
+                if (contact.CompareTag("Module"))
+                {
+                    Vector3 closestPoint = contact.ClosestPoint(transform.position);
+
+                    float distance = Vector3.Distance(Position, closestPoint);
+                    if (distance < closestDistance)
+                    {
+                        closestModule = contact;
+                        closestDistance = distance;
+                    }
+                }
+            }
+
+            if (closestModule == null) return;
+            
+            GameObject module = closestModule.gameObject;
+            Debug.Log("Player is inside module: " + module.gameObject.name);
+            
+            Input.SwitchCurrentActionMap($"{module.gameObject.name}");
+        }
+
+        public void OnExitModule()
+        {
+            Input.SwitchCurrentActionMap("Convoy");
+        }
+
+        #endregion
     }
 }
