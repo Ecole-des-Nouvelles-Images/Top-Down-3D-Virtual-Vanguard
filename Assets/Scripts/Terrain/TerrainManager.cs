4cd91d5e0ecfe0e91f30b4990b29f6185e8d18e7
Charlie Boyer <charlieboyer@outlook.fr>
2024-05-13 15:12:53 +0200

    Update TerrainManager to start some terrain management effects

--- Assets/Scripts/Terrain/TerrainManager.cs
+++ Assets/Scripts/Terrain/TerrainManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Terrain.Procedural;
 using UnityEngine;
 
@@ -6,6 +7,9 @@ namespace Terrain
 {
     public class TerrainManager : MonoBehaviour
     {
+        [Header("Gameplay Settings")]
+        public float SlowDownTransitionTime;
+        
         [Header("Experimental")]
         public MapGenerator Generator;
         public bool EnableScrolling;
@@ -17,6 +21,23 @@ namespace Terrain
             {
                 Generator.Offset.x += ScrollSpeed * Time.deltaTime;
                 Generator.GenerateMap();
+            }
+        }
+
+        public void StopScrolling()
+        {
+            StartCoroutine(SlowDownScrollingCoroutine());
+        }
+
+        private IEnumerator SlowDownScrollingCoroutine()
+        {
+            float t = 0f;
+
+            while (t > 0)
+            {
+                t += Time.deltaTime * SlowDownTransitionTime;
+                Generator.Offset.x = Mathf.Lerp(0, ScrollSpeed, t);
+                yield return null;
             }
         }
     }
